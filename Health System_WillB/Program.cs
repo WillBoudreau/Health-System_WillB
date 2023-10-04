ed398b544a077e6cd14d4924391f50d9e968aa96
WillBoudreau <williamboudreau804@gmail.com>
2023-10-04 17:43:01 -0400

    October 4th 2023 changes

--- Health System_WillB/Program.cs
+++ Health System_WillB/Program.cs
@@ -9,19 +9,25 @@ namespace Health_System_WillB
     internal class Program
     {
         //int variables 
-        int lives;
-        int xp;
-        int level;
+        static int lives;
+        static int xp;
+        static int level;
         //float variables
-        float health = 100f;
-        float shield;
-        float healthUp;
-        float shieldUp;
-        float damage;
-
-        static void Main()
+        static float health = 100f;
+        static float shield;
+        static float healthUp;
+        static float shieldUp;
+        static float damage;
+        static float attack;
+        static float monsterhealth;
+        // string varaibles
+        static string responce;
+        static void Main(string[] args)
         {
-            health = 100;
+            health = 100f;
+            lives = 3;
+            xp = 0;
+            shield = 100f;
             Console.WriteLine("From Will's Studio");
             Console.WriteLine("------------------");
             Console.ReadKey();
@@ -36,11 +42,62 @@ namespace Health_System_WillB
             Console.WriteLine("Press any key to begin (NOT ALT + F4 or ESC)");
             Console.ReadKey();
             ShowHUD();
+            Mission();
         }
          
-        void ShowHUD()
+        static void ShowHUD()
         {
-            Console.WriteLine("You have " + health + "health");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("You have " + health + " health");
+            Console.WriteLine("You have " + shield + " shield");
+            Console.WriteLine("You have " + lives + " lives");
+            Console.WriteLine("You have " + xp + " xp");
+            Console.WriteLine("-------------------------------");
+            Console.ReadKey();
+        }
+        static void Damage(float damage)
+        {
+            monsterhealth = 100;
+            damage = 10;
+            if (monsterhealth < 50)
+            {
+                damage += 10;
+            }
+            if( shield  > 0 )
+            {
+                shield -= damage;
+                if( shield <= 0)
+                {
+                    shield = 0;
+                }
+            }
+            else
+            {
+                health -= damage;
+                if ( health <= 0 ) 
+                {
+                    health = 0;
+                }
+            }
+        }
+        static void Monster()
+        {
+            Console.WriteLine("You are attacked by a monster!");
+            Damage(damage);
+        }
+        static void Mission()
+        {
+            Console.WriteLine("Would you like to begin? Y/N");
+            responce = Console.ReadLine();
+            if (responce == "Y")
+            {
+                ShowHUD();
+                Monster();
+            }
+            else
+            {
+                Console.WriteLine("Okay, see ya");
+            }
         }
     }
 }
