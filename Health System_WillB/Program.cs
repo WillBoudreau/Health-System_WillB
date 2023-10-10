0c5dad73ceb2bc28c42078964a1221d95802ae0d
WillBoudreau <williamboudreau804@gmail.com>
2023-10-10 11:45:59 -0400

    Changes made at October 10th 2023 at 11:45am

--- Health System_WillB/Program.cs
+++ Health System_WillB/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Health_System_WillB
 {
@@ -13,21 +14,24 @@ namespace Health_System_WillB
         static int xp;
         static int level;
         //float variables
-        static float health = 100f;
+        static float health;
         static float shield;
         static float healthUp;
         static float shieldUp;
         static float damage;
         static float attack;
         static float monsterhealth;
-        // string varaibles
-        static string responce;
+        static float scoremultiplyer;
         static void Main(string[] args)
         {
             health = 100f;
             lives = 3;
             xp = 0;
             shield = 100f;
+            level = 0;
+            attack = 25;
+            monsterhealth = 100;
+            scoremultiplyer = 2.5f;
             Console.WriteLine("From Will's Studio");
             Console.WriteLine("------------------");
             Console.ReadKey();
@@ -51,13 +55,14 @@ namespace Health_System_WillB
             Console.WriteLine("You have " + health + " health");
             Console.WriteLine("You have " + shield + " shield");
             Console.WriteLine("You have " + lives + " lives");
+            Console.WriteLine("You are at Level " + level);
             Console.WriteLine("You have " + xp + " xp");
             Console.WriteLine("-------------------------------");
             Console.ReadKey();
+            Monster();
         }
         static void Damage(float damage)
         {
-            monsterhealth = 100;
             damage = 10;
             if (monsterhealth < 50)
             {
@@ -76,7 +81,58 @@ namespace Health_System_WillB
                 health -= damage;
                 if ( health <= 0 ) 
                 {
+                    Console.WriteLine("You lose");
                     health = 0;
+                }
+                if ( health >0 && health < 10) 
+                {
+                    Console.WriteLine("Health Critical!");
+                    ShowHUD();
+                }
+                if (health >= 10 && health < 20)
+                {
+                    Console.WriteLine("Health really low!");
+                    ShowHUD();
+                }
+                if (health >= 20 && health < 30)
+                {
+                    Console.WriteLine("Health low");
+                    ShowHUD();
+                }
+                if (health >= 30 && health < 40) 
+                {
+                    Console.WriteLine("Health getting low");
+                    ShowHUD();
+                }
+                if (health >= 40 && health < 50)
+                {
+                    Console.WriteLine("Healh mid");
+                    ShowHUD();
+                }
+                if (health >= 50 && health < 60)
+                {
+                    Console.WriteLine("Health not bad");
+                    ShowHUD();
+                }
+                if (health >= 60 &&  health < 70)
+                {
+                    Console.WriteLine("Health okay");
+                    ShowHUD();
+                }
+                if (health >= 70 && health < 80)
+                {
+                    Console.WriteLine("Health Good");
+                    ShowHUD();
+                }
+                if (health >= 80 && health < 90)
+                {
+                    Console.WriteLine("Health Great!");
+                    ShowHUD();
+                }
+                if (health >= 90 && health <= 100)
+                {
+                    Console.WriteLine("Health Awesome!");
+                    ShowHUD();
                 }
             }
         }
@@ -84,21 +140,27 @@ namespace Health_System_WillB
         {
             Console.WriteLine("You are attacked by a monster!");
             Damage(damage);
+            Console.WriteLine("You fight back! with " + attack + " Attack");
+            monsterhealth -= attack;
+            Console.WriteLine("Monster health is at " + monsterhealth);
+            Console.ReadKey();
+            if (monsterhealth <= 0)
+            {
+                monsterhealth = 0;
+                xp = xp * scoremultiplyer;
+                Console.WriteLine("You win!");
+                Console.WriteLine("You get " + xp + "xp");
+            }
+            ShowHUD();
         }
         static void Mission()
         {
-            Console.WriteLine("Would you like to begin? Y/N");
-            responce = Console.ReadLine();
-            if (responce == "Y")
-            {
                 ShowHUD();
                 Monster();
-            }
-            else
-            {
-                Console.WriteLine("Okay, see ya");
-            }
         }
+        
+         
     }
+  
 }
 
